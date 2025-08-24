4eeb730e32d67c6643aa9bac0299536e17290522
thatguynoe <noereyesrivas@gmail.com>
2025-08-23 23:46:57 -0400

    use proper commenting for disabled blocks

--- config.h
+++ config.h
@@ -21,18 +21,16 @@
     X("", "cat /tmp/recordingicon 2>/dev/null",  0,  9) \
     X("", "music",        0,                  11)       \
     X("", "pacpackages",  0,                  8)        \
+    X("", "disk /",       10800,              6)        \
     X("", "memory",       10,                 14)       \
     X("", "cpu",          10,                 18)       \
+    /* X("", "moonphase",    18000,              17) */     \
+    /* X("", "weather",      18000,              5)  */     \
+    /* X("", "mailbox",      600,                12) */     \
     X("", "bright",       0,                  19)       \
-    X("", "mailbox",      600,                12)       \
     X("", "volume",       0,                  10)       \
     X("", "battery",      5,                  3)        \
     X("", "internet",     5,                  4)        \
     X("", "clock",        1,                  1)
 
 #endif  // CONFIG_H
-
-// Disabled blocks.
-    /*X("", "disk",         10800,              6)        \*/
-    /*X("", "moonphase",    18000,              17)       \*/
-    /*X("", "weather",      18000,              5)        \*/
